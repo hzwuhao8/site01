7a470c57465bad0d4ce1976d5956348ae7912b3e
wuhao <wuhao@bokee.net>
2018-04-21 22:54:52 +0800

    修改 连接 数据库的方式

--- www/base.php
+++ www/base.php
@@ -2,9 +2,9 @@
 require "WEB-INF/lib/adodb/adodb.inc.php";
 $driver="mysqli";
 $db= adoNewConnection ($driver);
-$parameters = "root:mypass01@mysql/site01";
+$parameters = "root:mypass01@mysqlserver01/site01";
 
-$db-> connect( $parameters);
+$db-> connect( "mysqlserver01", "root", "mypass01","site01" );
 if($_REQUEST['debug']){
  $db->debug = true;
 }
